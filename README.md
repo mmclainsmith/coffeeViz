da18335ded83db877de431123f2480fcaf89168d
Morgan <mrgnmcsmith@gmail.com>
2018-12-01 16:39:59 -0500

    Added fake data gen func

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # coffeeViz
+
+
+- store data twice, date (year) as key
+                        {month:{country_name:{prod, cons, export, stocks}}},
+                    country name as key
+                        {date:{month:{prod, cons, export, stocks}}}
+
+drawLineChart(country_name)
+
+
+1990/1991 -> 1991
---
+++ line_test_data.js
@@ -0,0 +1,21 @@
+//{date:{month:{prod, cons, export, stocks}}}
+
+function fakeData() {
+    let data = {}
+    let months = ['apr', 'jul', 'oct']
+    for (let year = 1990; year < 2018; year++) {
+        console.log(year)
+        yearData = {}
+        for (let i = 0; i < 3; i++){
+            let mData = {'prod': 1000 * Math.random(),
+                        'consum': 1000 * Math.random(),
+                        'export': 1000 * Math.random(),
+                        'stock': 1000 * Math.random()}
+            yearData[months[i]] = mData
+        }
+        data[year] = yearData
+    }
+    return data
+}
+
+data = fakeData()
